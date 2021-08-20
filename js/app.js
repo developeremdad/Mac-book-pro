c20c82ab3c37923441f7d2631f5ec469ea4ffa92
developeremdad <developeremdad@gmail.com>
2021-08-21 02:13:38 +0600

    subTotal Calculation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,57 @@
-console.log("hello");
+// select all needed handler id 
+let defaultMemoryPrice = document.getElementById('default-memory-price');
+let extraMemoryPrice = document.getElementById('extra-memory-price');
+
+let defaultStoragePrice = document.getElementById('default-storage-price');
+let mediumStoragePrice = document.getElementById('medium-storage-price');
+let maxStoragePrice = document.getElementById('max-storage-price');
+
+let freeDeliveryPrice = document.getElementById('free-delivery-price');
+let chargeDeliveryPrice = document.getElementById('charge-delivery-price');
+let promoButton = document.getElementById('promo-button');
+
+// select all display field
+let memeoryPriceField = document.getElementById('memory-price');
+let storagePriceField = document.getElementById('storage-price');
+let deliveryPriceField = document.getElementById('delivery-price');
+let subTotalField = document.getElementById('sub-total-price');
+let promoInputField = document.getElementById('promo-input-field');
+let totalField = document.getElementById('total');
+
+//function for add value in price field
+function addDefaultPrice(field, price) {
+    field.innerText = '0';
+    let subTotalText = subTotalField.innerText;
+    let subTotalFloat = parseFloat(subTotalText);
+    subTotalField.innerText = subTotalFloat - price;
+}
+//function for add value in price field with price
+function addCustomizePrice(field, price) {
+    field.innerText = price;
+    let subTotalText = subTotalField.innerText;
+    let subTotalFloat = parseFloat(subTotalText);
+    subTotalField.innerText = subTotalFloat + price;
+}
+// add event handler for default vlaue
+defaultMemoryPrice.addEventListener('click', function () {
+    addDefaultPrice(memeoryPriceField, 100);
+});
+defaultStoragePrice.addEventListener('click', function () {
+    addDefaultPrice(storagePriceField);
+});
+freeDeliveryPrice.addEventListener('click', function () {
+    addDefaultPrice(deliveryPriceField);
+});
+//add event handler for extra service
+extraMemoryPrice.addEventListener('click', function () {
+    addCustomizePrice(memeoryPriceField, 100);
+});
+mediumStoragePrice.addEventListener('click', function () {
+    addCustomizePrice(storagePriceField, 100);
+});
+maxStoragePrice.addEventListener('click', function () {
+    addCustomizePrice(storagePriceField, 180);
+});
+chargeDeliveryPrice.addEventListener('click', function () {
+    addCustomizePrice(deliveryPriceField, 100);
+});
