85410c5d98fd730700497c3a56677ab3eb4d4721
developeremdad <developeremdad@gmail.com>
2021-08-21 10:25:50 +0600

    subTotal final calculatio

--- js/app.js
+++ js/app.js
@@ -19,22 +19,36 @@ let promoInputField = document.getElementById('promo-input-field');
 let totalField = document.getElementById('total');
 
 //function for add value in price field
-function addDefaultPrice(field, price) {
+function addDefaultPrice(field) {
     field.innerText = '0';
-    let subTotalText = subTotalField.innerText;
-    let subTotalFloat = parseFloat(subTotalText);
-    subTotalField.innerText = subTotalFloat - price;
+    subTotalCalc();
 }
+
+//get field value
+function getFieldValue(field) {
+    let fieldText = field.innerText;
+    let fieldFloat = parseFloat(fieldText);
+    return fieldFloat;
+}
+
 //function for add value in price field with price
 function addCustomizePrice(field, price) {
-    field.innerText = price;
     let subTotalText = subTotalField.innerText;
-    let subTotalFloat = parseFloat(subTotalText);
-    subTotalField.innerText = subTotalFloat + price;
+
+    field.innerText = price;
+    subTotalCalc();
+    
 }
+
+//calculate sub total
+function subTotalCalc() {
+    let subTotal = 1299 +  getFieldValue(memeoryPriceField) + getFieldValue(storagePriceField) + getFieldValue(deliveryPriceField);
+    subTotalField.innerText = subTotal;
+}
+
 // add event handler for default vlaue
 defaultMemoryPrice.addEventListener('click', function () {
-    addDefaultPrice(memeoryPriceField, 100);
+    addDefaultPrice(memeoryPriceField);
 });
 defaultStoragePrice.addEventListener('click', function () {
     addDefaultPrice(storagePriceField);
@@ -42,6 +56,7 @@ defaultStoragePrice.addEventListener('click', function () {
 freeDeliveryPrice.addEventListener('click', function () {
     addDefaultPrice(deliveryPriceField);
 });
+
 //add event handler for extra service
 extraMemoryPrice.addEventListener('click', function () {
     addCustomizePrice(memeoryPriceField, 100);
